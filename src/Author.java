d5da6921e079afa4d9432fd22665cc2de72d8fd8
PrachiPhatak <phatak.prachi@gmail.com>
2021-07-13 12:09:26 -0500

    Adds suppresswarnings

--- src/Author.java
+++ src/Author.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
+    @SuppressWarnings("all")
     private List books;
 
+    
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
         books = new ArrayList();
@@ -28,7 +30,6 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
